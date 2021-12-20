39c4e2b572a8476c1860f6e7c6cc209cc829cce8
fredrik1998 <fredrik.andren@student.kyh.se>
2021-12-20 15:02:08 +0100

    Made Contact page responsive

--- script.js
+++ script.js
@@ -34,5 +34,15 @@ $(document).ready(function(){
     });
 
     // owl carousel script
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
   
 });
+
