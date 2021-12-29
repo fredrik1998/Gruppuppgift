1a4ca452282bfbc4feb83ac78cd3096745132100
fredrik1998 <fredrik.andren@student.kyh.se>
2021-12-29 10:23:15 +0100

    Added some styling changes and cleaned the code

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ $(document).ready(function(){
         }else{
             $('nav').removeClass(".bg-transparent");
         }
-        
-        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -30,16 +28,13 @@ $(document).ready(function(){
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
